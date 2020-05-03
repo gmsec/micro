42183373b174f8f7969404e4d09aab97d1351c3c
xxjwxc <xie1xiao1jun@qq.com>
2020-05-03 13:51:24 +0800

    remove log
    移除log

--- client/grpc_pool.go
+++ client/grpc_pool.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"fmt"
 	"sync"
 	"time"
 
@@ -76,7 +75,6 @@ func (p *pool) getConn(addr string, opts ...grpc.DialOption) (*poolConn, error) 
 		p.conns[addr] = sp
 	}
 
-	fmt.Println("=======pool_len:", sp.count)
 	//  while we have conns check streams and then return one
 	//  otherwise we'll create a new conn
 	conn := sp.head.next
